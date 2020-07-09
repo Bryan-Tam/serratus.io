3d37162b71f8913a18a857b90ef8a8bad7403982
lohrd <57124913+lohrd@users.noreply.github.com>
2020-07-09 15:47:48 -0400

    "visual" test component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,7 +45,7 @@ const Navbar = () => {
            </div>
             </div>
             {/* }   */}
-        </div>
+        </div> 
     )
 }
 
--- src/pages/Data.js
+++ src/pages/Data.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Axios from 'axios';
 import DataSdk from '../SDK/DataSdk';
-import { get } from 'react-scroll/modules/mixins/scroller';
+import Visual from'../components/Visual';
 
 const  Data = (props) => {
     const [searchString, setSearchString] = React.useState("")
@@ -35,26 +34,32 @@ const  Data = (props) => {
     }
     
     return (
-        <div className="h-screen w-screen flex flex-col items-center bg-blue-500 justify-center p-6">
-            <div class="w-1/2 h-64 flex flex-col justify-center">
+        <div className="h-screen w-screen flex flex-col items-center justify-center">
+            {/* <Visual></Visual> */}
+            <img src="/serratus.jpg" className="invisible sm:visible opacity-75 sm:fixed" style={{objectFit: 'cover', minWidth: '100vh', minHeight: '100vh'}} />
 
-           
-            <div class="flex flex-col justify-center items-center">
-                {data.sra 
-                ? 
-                <div>SRA: {data.sra}</div> 
-                : 
-                <div>SRA: </div>}
-                {heatMap ? 
-                <img src={heatMap} className="h-full w-full p-6 m-6"></img>
-                : 
-                <div>{imageError}</div> }
-                <div class="flex flex-col justify-center items-center">
-                    <input class="p-2 border-black border-2 rounded-lg" onChange={e => setSearchString(e.target.value)}></input>
-                    <button class="" onClick={() => genSra()} className="h-10 w-32 border-black border-2 rounded-lg" title="get heat">Search</button>
-                </div>
-                <a src={`https://www.ncbi.nlm.nih.gov/sra/?term=${data.sra}`}></a>
-            </div>
+            <div class="w-1/2 h-full mt-40 mb-24 flex flex-col justify-center items-center z-10 bg-white rounded-lg shadow-2xl">
+
+            <div className="w-4/5 bg-gray-500 h-full"></div>
+            <div className="w-4/5 bg-gray-600 h-full"></div>
+            <div className="w-4/5 bg-gray-700 h-full"></div>
+
+                {/* <div class="flex flex-col justify-center items-center">
+                    {data.sra 
+                    ? 
+                    <div>SRA: {data.sra}</div> 
+                    : 
+                    <div>SRA: </div>}
+                    {heatMap ? 
+                    <img src={heatMap} className="h-full w-full p-6 m-6"></img>
+                    : 
+                    <div>{imageError}</div> }
+                    <div class="flex flex-col justify-center items-center">
+                        <input class="p-2 border-black border-2 rounded-lg" onChange={e => setSearchString(e.target.value)}></input>
+                        <button class="" onClick={() => genSra()} className="h-10 w-32 border-black border-2 rounded-lg" title="get heat">Search</button>
+                    </div>
+                    <a src={`https://www.ncbi.nlm.nih.gov/sra/?term=${data.sra}`}></a>
+                </div> */}
             </div>
         </div>
     )
